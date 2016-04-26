999151cdf53b965e257968564010601743bfaf2b
Thiago <thiagoisaiasbrg@gmail.com>
2016-04-25 21:32:31 -0300

    CREATED: Program , MainClass, ClassDeclSimple, ClassDeclExtends and VarDecl #12

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,28 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import syntaxtree.Program;
+import visitor.PrettyPrintVisitor;
+
 public class Main {
-   
-   public static void main(String [] args) throws FileNotFoundException {
-      try {
-  		 File file = new File("src/file.txt");
-         new MiniJavaParser(new FileInputStream(file)).Program();
-         System.out.println("Lexical analysis successfull");
-      }
-      catch (ParseException e) {
-         System.out.println("Lexer Error : \n"+ e.toString());
-      }
-   }
+
+	public static void main(String [] args) throws FileNotFoundException {
+		try {
+			File file = new File("src/file.txt");
+
+			Program root = new MiniJavaParser(new FileInputStream(file)).Goal();
+			System.out.println("Lexical analysis successfull");
+
+			// Print the original source code from the abstact syntax tree:
+			root.accept(new PrettyPrintVisitor());
+			// Print the abstract syntax tree:
+			//root.accept(new ASTPrintVisitor());
+			// Should this have been called "UglyPrintVisitor"? :)
+		}
+		catch (ParseException e) {
+			System.out.println(e.toString());
+		}
+	}
 }
 
 
