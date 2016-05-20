cf8a80b3733aa32436b3968b7d4064d3745808b1
Thiago <thiagoisaiasbrg@gmail.com>
2016-05-20 13:01:52 -0300

    BuildSymbolTableVisitor working

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,25 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-import syntaxtree.Program;
-import visitor.PrettyPrintVisitor;
+import syntaxtree.*;
+import visitor.*;
+import symboltable.*;
 
 public class Main {
-
-	public static void main(String [] args) throws FileNotFoundException {
-		try {
-			File file = new File("src/file3.txt");
-
-			Program root = new MiniJavaParser(new FileInputStream(file)).Goal();
-			System.out.println("Lexical analysis successfull");
-
-			// Print the original source code from the abstact syntax tree:
-			root.accept(new PrettyPrintVisitor());
-			// Print the abstract syntax tree:
-			//root.accept(new ASTPrintVisitor());
-			// Should this have been called "UglyPrintVisitor"? :)
-		}
-		catch (ParseException e) {
-			System.out.println(e.toString());
-		}
+    public static void main(String [] args) {
+	try {
+		
+		File file = new File("src/file2.txt");
+		Program root = new MiniJavaParser(new FileInputStream(file)).Goal();
+		
+	    BuildSymbolTableVisitor bstv = new BuildSymbolTableVisitor();
+	    root.accept(bstv);
+	    System.out.println(bstv.symbolTable);
 	}
+	catch (ParseException e) {
+	    System.out.println(e.toString());
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+    }
 }
-
-
