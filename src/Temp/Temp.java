9080c9d83a49f04540e472db7d460cbe7a6a141b
Leo Linhares <slinharesleo@gmail.com>
2016-06-06 20:55:17 -0300

    added files to the temp package

---
+++ src/Temp/Temp.java
@@ -0,0 +1,1 @@
+package Temp;public class Temp{    private static int count=30;    private int num;    public String toString() {return "t" + num;}    public Temp() { num = count++; }    public Temp(int t){    num = t ;}    public int hashCode() { return num; }    public boolean spillTemp = false;    public int spillCost;}
